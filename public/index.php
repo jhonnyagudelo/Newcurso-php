9b2c7d76da8bc6d7075aa3f47fe9d3cc2138827f
jhonny stiven agudelo tenorio <jhonny.agudelo3120@gmail.com>
2019-08-24 00:26:23 -0500

    'envModificado2'

--- public/index.php
+++ public/index.php
@@ -11,6 +11,9 @@
 			$dotenv = Dotenv\Dotenv::create(__DIR__ . '/..');
 			$dotenv->load();
 
+			echo getenv('DB_HOST');
+			die;
+
 			use Illuminate\Database\Capsule\Manager as Capsule;
 			use Aura\Router\RouterContainer;
 			use Zend\Diactoros\Response\HtmlResponse;
